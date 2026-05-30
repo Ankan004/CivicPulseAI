256a422d2afdc8582a46c8b5b8511dbcdd8ffe3f
Ankan Ghosh <ankan12122004@gmail.com>
2026-05-30 22:57:43 +0530

    Implemented user registration system

--- backend/main.py
+++ backend/main.py
@@ -5,12 +5,16 @@ from app.database.session import engine
 
 from app.models.user import User
 
+from app.api.auth import router as auth_router
+
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="CivicPulse AI"
 )
+
+app.include_router(auth_router)
 
 
 @app.get("/")
