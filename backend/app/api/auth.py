e0e001e26295bd2559784cd760f9baacd30644c4
Ankan Ghosh <ankan12122004@gmail.com>
2026-05-31 10:31:14 +0530

    Implemented JWT authentication

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -5,12 +5,20 @@ from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from app.models.user import User
-from app.schemas.user import UserCreate
-from app.schemas.user import UserResponse
+
+from app.schemas.user import (
+    UserCreate,
+    UserResponse,
+    UserLogin
+)
 
 from app.database.dependencies import get_db
 
-from app.core.security import hash_password
+from app.core.security import (
+    hash_password,
+    verify_password,
+    create_access_token
+)
 
 
 router = APIRouter(
@@ -49,3 +57,40 @@ def register(
     db.refresh(new_user)
 
     return new_user
+
+
+@router.post("/login")
+def login(
+    user: UserLogin,
+    db: Session = Depends(get_db)
+):
+
+    db_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if not db_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials"
+        )
+
+    if not verify_password(
+        user.password,
+        db_user.password
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials"
+        )
+
+    access_token = create_access_token(
+        {
+            "sub": db_user.email
+        }
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
